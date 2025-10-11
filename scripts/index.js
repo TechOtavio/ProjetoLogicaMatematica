ec8bf0c67020a245eee9d5d26520a5acd710863a
Otávio Levi <otavio.holanda05@gmail.com>
2025-10-11 00:53:16 -0300

    Adicionando função aos inputs, editando o css e adicionando uma div para resposta de login.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,22 @@
-let estadoBotao = false;
+let inputEmail = document.getElementById('email')
+let inputSenha = document.getElementById('senha')
 let botao = document.getElementById('botaoAtivado') || document.getElementById('botaoDesativado');
+let pErro = document.getElementById('textoErro')
+
+let email = '';
+let senha = '';
+let valorInputEmail;
+let valorInputSenha;
+let estadoBotao = false;
+
+
+inputEmail.addEventListener('change', (event) => {
+    valorInputEmail = event.target.value;
+})
+
+inputSenha.addEventListener('change', (event) => {
+    valorInputSenha = event.target.value;
+})
 
 botao.addEventListener('click', (event) => {
     event.preventDefault();
@@ -12,4 +29,8 @@ botao.addEventListener('click', (event) => {
         botao.id= 'botaoAtivado';
         estadoBotao = true;
     }
-})  
+
+    if(valorInputEmail != email || valorInputSenha != senha){
+        pErro.textContent = `Credenciamento inválido, cadastre-se ou tente novamente!`
+    }
+})
